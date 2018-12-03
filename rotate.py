8da9ae29e597a4e43a80dad119c4cfc5828122b9
Nirpa Hang Rai <nirparai@yahoo.com>
2018-12-03 10:32:20 +0545

    front face rotation

--- rotate.py
+++ rotate.py
@@ -1,14 +1,23 @@
 import numpy as np
-def front_rotate_clockwise(rubix_array):
-	# print(rubix_array[0])
-	rotated_face = np.roll(rubix_array, 3)
-	print(rotated_face)
+import random
 
+def front_anti_clockwise(rubix_array):
+	front_face = np.reshape(rubix_array[0], (3, 3))
+	anti_rotated_front_face = np.rot90(front_face)
+	print(anti_rotated_front_face)
+
+def front_clockwise(rubix_array):
+	front_face = np.reshape(rubix_array[0], (3, 3))
+	rotated_front_face = np.rot90(front_face, -1)
+	print(rotated_front_face)
+	
 if __name__ == '__main__':
 	rubix_array = np.zeros([6,9], dtype = str)
 	color_array = ['W','G','Y','R','B','O']
-	for faces in range(0,6):
+	for faces in xrange(0,6):
 		for pieces in range(0,9):
-			rubix_array[faces][pieces] = color_array[faces]
-	front_rotate_clockwise(rubix_array)
-	
+			rubix_array[faces][pieces] = color_array[random.randrange(1,6)]
+	print(rubix_array)
+	front_anti_clockwise(rubix_array)
+	print()
+	front_clockwise(rubix_array)	
