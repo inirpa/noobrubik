87e454d6e0d522eafb4822211ee8c3faf091c504
Nirpa Hang Rai <nirparai@yahoo.com>
2018-12-03 13:40:28 +0545

    minified front inverse

--- rotate.py
+++ rotate.py
@@ -1,43 +1,16 @@
 import numpy as np
 import random
 
-def front_anti_clockwise(rubix_array):
-	front_face = np.reshape(rubix_array[0], (3, 3))
-	right_face = np.reshape(rubix_array[1], (3, 3))
-	back_face = np.reshape(rubix_array[2], (3, 3))
-	left_face = np.reshape(rubix_array[3], (3, 3))
-	up_face = np.reshape(rubix_array[4], (3, 3))
-	down_face = np.reshape(rubix_array[5], (3, 3))
+def front_anti_clockwise(front_face, right_face, back_face, left_face, up_face, down_face):
 	anti_rotated_front_face = np.rot90(front_face)
-	print("F' roated")
-	print(anti_rotated_front_face)
-
-	print("Right First Columns")
-	right_first_col = right_face[:, 0]
-	print(right_first_col)
-
-	print("Left Last Columns")
-	left_last_col = left_face[:, 2]
-	print(left_last_col)
-
-	print("Up Last Row")
-	up_last_row = up_face[2, :]
-	print(up_last_row)
-
-	print("Down First Row")
-	down_first_row = down_face[0, :]
-	print(down_first_row)
 
 	temp_face = np.zeros([1, 3], dtype = int)
-	temp_face = right_first_col
-	print(temp_face)
 
-	right_face[:, 0] = down_first_row
-	down_face[0, :] = left_last_col
-	left_face[:, 2] = up_last_row
+	temp_face = right_face[:, 0]
+	right_face[:, 0] = down_face[0, :]
+	down_face[0, :] = left_face[:, 2]
+	left_face[:, 2] = up_face[2, :]
 	up_face[2, :] = temp_face
-	# print(right_face)
-
 
 	rotated_cube = [
 					anti_rotated_front_face,
@@ -64,5 +37,10 @@ if __name__ == '__main__':
 	print("Orgial Scrambled Cube")
 	print(rubix_array)
 	print("Face rotate clockwise F'")
-	front_anti_clockwise(rubix_array)
-	# front_clockwise(rubix_array)
+	front_face = np.reshape(rubix_array[0], (3, 3))
+	right_face = np.reshape(rubix_array[1], (3, 3))
+	back_face = np.reshape(rubix_array[2], (3, 3))
+	left_face = np.reshape(rubix_array[3], (3, 3))
+	up_face = np.reshape(rubix_array[4], (3, 3))
+	down_face = np.reshape(rubix_array[5], (3, 3))
+	print(front_anti_clockwise(front_face, right_face, back_face, left_face, up_face, down_face))
