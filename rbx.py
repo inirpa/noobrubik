34239d1aef0c83dc3e11d921b1a987e35c4181ee
Nirpa Hang Rai <nirparai@yahoo.com>
2018-12-03 17:28:18 +0545

    rotate separated

--- rbx.py
+++ rbx.py
@@ -1,18 +1,22 @@
-def clockturn():
-	print('clock wise turn')
-	face = 
-		[
-			['a','b','c'],
-			['d','e','f'],
-			['g','h','i']
-		]
-	temp_face = face
-	print(type(face))
-	print(face[1][1])
+import numpy as np
+import rotate
+import random
 
-	
-
-
-
-if __name__ == "__main__":
-	clockturn()
+if __name__ == '__main__':
+	rubix_array = np.zeros([6,9], dtype = str)
+	color_array = ['W','G','Y','R','B','O']
+	for faces in xrange(0,6):
+		for pieces in range(0,9):
+			rubix_array[faces][pieces] = color_array[random.randrange(1,6)]
+	print("Orgial Scrambled Cube")
+	print(rubix_array)
+	front_face = np.reshape(rubix_array[0], (3, 3))
+	right_face = np.reshape(rubix_array[1], (3, 3))
+	back_face = np.reshape(rubix_array[2], (3, 3))
+	left_face = np.reshape(rubix_array[3], (3, 3))
+	up_face = np.reshape(rubix_array[4], (3, 3))
+	down_face = np.reshape(rubix_array[5], (3, 3))
+	print("Face rotate anti-clockwise F'")
+	print(np.reshape(front_anti_clockwise(front_face, right_face, back_face, left_face, up_face, down_face), (6, 9)))
+	print("Face rotate clockwise F")
+	print(np.reshape(front_clockwise(front_face, right_face, back_face, left_face, up_face, down_face),(6, 9)))
--- rotate.py
+++ rotate.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 
 def front_anti_clockwise(front_face, right_face, back_face, left_face, up_face, down_face):
 	anti_rotated_front_face = np.rot90(front_face)
@@ -23,25 +22,4 @@ def front_clockwise(front_face, right_face, back_face, left_face, up_face, down_
 	up_face[2, :] = left_face[:, 2]
 	left_face[:, 2] = down_face[0, :]
 	down_face[0, :] = temp_face
-
-	return(rotated_front_face, right_face, back_face, left_face, up_face, down_face)	
-	
-
-if __name__ == '__main__':
-	rubix_array = np.zeros([6,9], dtype = str)
-	color_array = ['W','G','Y','R','B','O']
-	for faces in xrange(0,6):
-		for pieces in range(0,9):
-			rubix_array[faces][pieces] = color_array[random.randrange(1,6)]
-	print("Orgial Scrambled Cube")
-	print(rubix_array)
-	front_face = np.reshape(rubix_array[0], (3, 3))
-	right_face = np.reshape(rubix_array[1], (3, 3))
-	back_face = np.reshape(rubix_array[2], (3, 3))
-	left_face = np.reshape(rubix_array[3], (3, 3))
-	up_face = np.reshape(rubix_array[4], (3, 3))
-	down_face = np.reshape(rubix_array[5], (3, 3))
-	print("Face rotate anti-clockwise F'")
-	print(np.reshape(front_anti_clockwise(front_face, right_face, back_face, left_face, up_face, down_face), (6, 9)))
-	print("Face rotate clockwise F")
-	print(np.reshape(front_clockwise(front_face, right_face, back_face, left_face, up_face, down_face),(6, 9)))
+	return(rotated_front_face, right_face, back_face, left_face, up_face, down_face)
